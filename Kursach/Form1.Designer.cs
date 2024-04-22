f32d69e850081e7eda6c44feea066bd60665d285
LAPTOP-JF958PDO\mm121 <mm12112004@mail.ru>
2024-04-22 11:59:11 +0300

    Добавьте файлы проекта.

---
+++ Kursach/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Kursach
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.OpenImage = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.CreateGIF = new System.Windows.Forms.Button();
+            this.DelayLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.delayNum = new System.Windows.Forms.TextBox();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.insertButton = new System.Windows.Forms.Button();
+            this.GIFPictureBox = new System.Windows.Forms.PictureBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GIFPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OpenImage
+            // 
+            this.OpenImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.OpenImage.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OpenImage.Location = new System.Drawing.Point(31, 431);
+            this.OpenImage.Name = "OpenImage";
+            this.OpenImage.Size = new System.Drawing.Size(120, 35);
+            this.OpenImage.TabIndex = 1;
+            this.OpenImage.Text = "Добавить";
+            this.OpenImage.UseVisualStyleBackColor = true;
+            this.OpenImage.Click += new System.EventHandler(this.OpenImage_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "1.png");
+            this.imageList1.Images.SetKeyName(1, "2.gif");
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(31, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(266, 413);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // CreateGIF
+            // 
+            this.CreateGIF.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.CreateGIF.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.CreateGIF.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.CreateGIF.Font = new System.Drawing.Font("Arial", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CreateGIF.Location = new System.Drawing.Point(214, 393);
+            this.CreateGIF.Name = "CreateGIF";
+            this.CreateGIF.Size = new System.Drawing.Size(97, 35);
+            this.CreateGIF.TabIndex = 6;
+            this.CreateGIF.Text = "Создать";
+            this.CreateGIF.UseVisualStyleBackColor = false;
+            this.CreateGIF.Click += new System.EventHandler(this.CreateGIF_Click);
+            // 
+            // DelayLabel
+            // 
+            this.DelayLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.DelayLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.DelayLabel.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DelayLabel.Location = new System.Drawing.Point(-5, 399);
+            this.DelayLabel.Name = "DelayLabel";
+            this.DelayLabel.Size = new System.Drawing.Size(125, 29);
+            this.DelayLabel.TabIndex = 7;
+            this.DelayLabel.Text = "Задержка";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.saveButton);
+            this.groupBox1.Controls.Add(this.delayNum);
+            this.groupBox1.Controls.Add(this.CreateGIF);
+            this.groupBox1.Controls.Add(this.DelayLabel);
+            this.groupBox1.Location = new System.Drawing.Point(326, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(438, 434);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.saveButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.saveButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.saveButton.Font = new System.Drawing.Font("Arial", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.saveButton.Location = new System.Drawing.Point(317, 393);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(115, 35);
+            this.saveButton.TabIndex = 15;
+            this.saveButton.Text = "Сохранить";
+            this.saveButton.UseVisualStyleBackColor = false;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // delayNum
+            // 
+            this.delayNum.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.delayNum.Font = new System.Drawing.Font("Arial", 18F);
+            this.delayNum.Location = new System.Drawing.Point(121, 393);
+            this.delayNum.Name = "delayNum";
+            this.delayNum.Size = new System.Drawing.Size(87, 35);
+            this.delayNum.TabIndex = 14;
+            this.delayNum.Text = "33";
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.deleteButton.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.deleteButton.Location = new System.Drawing.Point(174, 486);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(107, 35);
+            this.deleteButton.TabIndex = 10;
+            this.deleteButton.Text = "Удалить";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Font = new System.Drawing.Font("Arial", 18F);
+            this.textBox1.Location = new System.Drawing.Point(277, 486);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(20, 35);
+            this.textBox1.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox2.Font = new System.Drawing.Font("Arial", 18F);
+            this.textBox2.Location = new System.Drawing.Point(278, 431);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(20, 35);
+            this.textBox2.TabIndex = 13;
+            // 
+            // insertButton
+            // 
+            this.insertButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.insertButton.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.insertButton.Location = new System.Drawing.Point(174, 431);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(112, 35);
+            this.insertButton.TabIndex = 12;
+            this.insertButton.Text = "Вставить";
+            this.insertButton.UseVisualStyleBackColor = true;
+            this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+            // 
+            // GIFPictureBox
+            // 
+            this.GIFPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GIFPictureBox.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.GIFPictureBox.Location = new System.Drawing.Point(326, 12);
+            this.GIFPictureBox.Name = "GIFPictureBox";
+            this.GIFPictureBox.Size = new System.Drawing.Size(432, 413);
+            this.GIFPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.GIFPictureBox.TabIndex = 2;
+            this.GIFPictureBox.TabStop = false;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.clearButton.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clearButton.Location = new System.Drawing.Point(31, 486);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(120, 35);
+            this.clearButton.TabIndex = 14;
+            this.clearButton.Text = "Очистить";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(787, 538);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.insertButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.GIFPictureBox);
+            this.Controls.Add(this.OpenImage);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GIFPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button OpenImage;
+        private System.Windows.Forms.PictureBox GIFPictureBox;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button CreateGIF;
+        private System.Windows.Forms.Label DelayLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button insertButton;
+        private System.Windows.Forms.TextBox delayNum;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Kursach/Form1.cs
@@ -0,0 +1,183 @@
+﻿using BumpKit;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Kursach
+{
+    public partial class Form1 : Form
+    {
+        public static List<string> imagesLinks = new List<string>();
+        ImageList imgList = new ImageList();
+
+        public Form1()
+        {
+            //Выбор размера отображаемых в списке изображений, только визуал
+            imgList.ImageSize = new Size(128, 128); 
+            InitializeComponent();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            //освобождаем память и удаляем дамп-файл
+            if (File.Exists("dump.gif"))
+            {
+                if (GIFPictureBox.Image != null)
+                    GIFPictureBox.Image.Dispose();
+                File.Delete("dump.gif");
+            }
+        }
+
+        private void OpenImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image Files(*.BMP;*.PNG;*.JPG)|*.BMP;*.PNG;*.JPG|All files (*.*)|*.*";
+            if (open.ShowDialog() != System.Windows.Forms.DialogResult.OK) { return; }
+
+            //imagesLinks - ссылка на картинки img - лист изображений
+            imagesLinks.Add(open.FileName);
+            imgList.Images.Add(Image.FromFile(imagesLinks[imagesLinks.Count-1]));
+
+            listView1.Items.Add((imagesLinks.Count).ToString(), imagesLinks.Count - 1);
+            listView1.LargeImageList = imgList;
+        }
+        
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (imagesLinks.Count == 0)
+            {
+                MessageBox.Show("Сначала введите первый элемент");
+            }
+            
+            else if (Convert.ToInt32(textBox1.Text) > 0)
+            {
+                //imagesLinks - ссылка на картинки img - лист изображений
+                imgList.Images.Clear();
+                imagesLinks.RemoveAt(Convert.ToInt32(textBox1.Text)-1);
+
+                foreach (var item in imagesLinks)
+                    imgList.Images.Add(Image.FromFile(item));
+                
+                listView1.Items.Clear();
+
+                for (int i = 0; i < imagesLinks.Count; i++)
+                {
+                    listView1.Items.Add((i + 1).ToString(), i);
+                }
+
+                listView1.LargeImageList = imgList;
+            }
+
+            else
+            {
+                MessageBox.Show("Введите значение в пределах диапазона от 1 до " + imagesLinks.Count);
+            }
+        }
+
+        private void insertButton_Click(object sender, EventArgs e)
+        {
+            if (imagesLinks.Count == 0)
+            {
+                MessageBox.Show("Сначала введите первый элемент");
+            }
+            
+            else if (Convert.ToInt32(textBox2.Text) < imagesLinks.Count && Convert.ToInt32(textBox2.Text) > 0)
+            {
+                OpenFileDialog open = new OpenFileDialog();
+                open.Filter = "Image Files(*.BMP;*.PNG;*.JPG)|*.BMP;*.PNG;*.JPG|All files (*.*)|*.*";
+                if (open.ShowDialog() != System.Windows.Forms.DialogResult.OK) { return; }
+
+                //imagesLinks - ссылка на картинки img - лист изображений
+                imgList.Images.Clear();
+                imagesLinks.Insert(Convert.ToInt32(textBox2.Text) - 1, open.FileName);
+
+                foreach (var item in imagesLinks)
+                    imgList.Images.Add(Image.FromFile(item));
+
+                listView1.Items.Clear();
+
+                for (int i = 0; i < imagesLinks.Count; i++)
+                {
+                    listView1.Items.Add((i + 1).ToString(), i);
+                }
+
+                listView1.LargeImageList = imgList;
+            }
+
+            else
+            {
+                MessageBox.Show("Введите значение в пределах диапазона от 1 до " + imagesLinks.Count);
+            }
+
+        }
+        private void CreateGIF_Click(object sender, EventArgs e)
+        {
+            if (GIFPictureBox.Image != null)
+            {
+                GIFPictureBox.Image.Dispose();
+            }
+            
+            if (imagesLinks.Count > 0)
+            {
+                double delay;
+                if (delayNum.Text != "")
+                {
+                    delay = Convert.ToDouble(delayNum.Text);
+                }
+                else
+                {
+                    delay = 33;
+                }
+
+
+                using (var gif = File.OpenWrite("dump.gif"))
+                {
+                    using (var encoder = new GifEncoder(gif))
+                        for (var i = 0; i < imagesLinks.Count; i += 1)
+                        {
+                            using (var frame = Image.FromFile(imagesLinks[i]).Rotate(0, false))
+                            {
+                                encoder.FrameDelay = TimeSpan.FromMilliseconds(delay);
+                                encoder.AddFrame(frame);
+                            }
+                        }
+                }
+
+                GIFPictureBox.Image = Image.FromFile("dump.gif");
+            }
+            else
+            {
+                MessageBox.Show("Выберите изображения!");
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            listView1.Clear();
+            imgList.Dispose();
+            imagesLinks.Clear();
+            
+            if (GIFPictureBox.Image != null)
+            {
+                GIFPictureBox.Image.Dispose();
+                GIFPictureBox.Image = null;
+            }
+
+            delayNum.Text = "33";
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Image Files(*.GIF)|*.GIF|All files (*.*)|*.*";
+            if (save.ShowDialog() != System.Windows.Forms.DialogResult.OK) { return; }
+
+            var img = Image.FromFile("dump.gif");
+            img.Save(save.FileName);
+        }
+    }
+}
